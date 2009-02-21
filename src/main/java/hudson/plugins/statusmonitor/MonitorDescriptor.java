441a15a0dd019f93e12ad1b703f62e3edad6bb8b
danielgalan <danielgalan@dev.java.net>
2009-02-21 18:08:58 +0000

    path corrected

--- src/main/java/hudson/plugins/statusmonitor/MonitorDescriptor.java
+++ src/main/java/hudson/plugins/statusmonitor/MonitorDescriptor.java
@@ -10,8 +10,8 @@ import org.kohsuke.stapler.StaplerRequest;
 
 public class MonitorDescriptor extends Descriptor<Publisher> {
 
-	public static final String ACTION_LOGO_LARGE = "/plugin/monitor/icons/monitor-32x32.png";
-	public static final String ACTION_LOGO_MEDIUM = "/plugin/monitor/icons/monitor-22x22.png";
+	public static final String ACTION_LOGO_LARGE = "/plugin/statusmonitor/icons/monitor-32x32.png";
+	public static final String ACTION_LOGO_MEDIUM = "/plugin/statusmonitor/icons/monitor-22x22.png";
 
 
 	protected MonitorDescriptor() {
