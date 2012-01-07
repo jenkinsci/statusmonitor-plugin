550c560b8b52bd9a8a24b141507e6629cc963f9c
Nicolas Peters <peters.nico@gmail.com>
2012-01-07 02:07:46 +0100

    Add a sound when the build status change to fail or success

--- src/main/java/hudson/plugins/statusmonitor/MonitorAction.java
+++ src/main/java/hudson/plugins/statusmonitor/MonitorAction.java
@@ -7,88 +7,113 @@ import org.kohsuke.stapler.export.ExportedBean;
 
 import javax.servlet.jsp.jstl.core.LoopTagStatus;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * Status Monitor, shows the configured Jobs in a single screen overview
  * 
- * @author Daniel Galán y Martins
+ * @author Daniel Galï¿½n y Martins
  */
-@ExportedBean (defaultVisibility = 999)
+@ExportedBean(defaultVisibility = 999)
 @Extension
 public class MonitorAction implements RootAction {
 	private static final long serialVersionUID = 1L;
 
 	private static final int COLUMNS = 2;
 
+	private static final int REFRESH_PAGE_TIME = 30;
 
 	public String getDisplayName() {
 		// The Name on the Dashboard
 		return "Status Monitor";
 	}
 
-
 	public String getIconFileName() {
 		return MonitorDescriptor.ACTION_LOGO_MEDIUM;
 	}
-
 
 	public String getUrlName() {
 		// The name of the URL path segment
 		return "/monitor";
 	}
 
-
 	/**
 	 * @return list projects that will be displayed
 	 */
 	private List<AbstractProject> getProjects() {
-        List<AbstractProject> result = new ArrayList<AbstractProject>();
+		List<AbstractProject> result = new ArrayList<AbstractProject>();
 		List<TopLevelItem> topLevelItems = Hudson.getInstance().getItems();
-        for (TopLevelItem topLevelItem : topLevelItems) {
-            if (topLevelItem instanceof AbstractProject) {
-                AbstractProject abstractProject = (AbstractProject) topLevelItem;
-                if (abstractProject.getPublishersList().get(MonitorPublisher.DESCRIPTOR) != null) {
-                        result.add(abstractProject);
-                }
-            }
-        }
+		for (TopLevelItem topLevelItem : topLevelItems) {
+			if (topLevelItem instanceof AbstractProject) {
+				AbstractProject abstractProject = (AbstractProject) topLevelItem;
+				if (abstractProject.getPublishersList().get(
+						MonitorPublisher.DESCRIPTOR) != null) {
+					result.add(abstractProject);
+				}
+			}
+		}
 
 		return result;
 	}
 
-
 	public String getResult(AbstractProject project) {
 		String result;
-		if ((project.getLastCompletedBuild() != null) && (project.getLastCompletedBuild().getResult() != null)) {
+		if ((project.getLastCompletedBuild() != null)
+				&& (project.getLastCompletedBuild().getResult() != null)) {
 			if (project.isDisabled()) {
 				result = "DISABLED";
-			}
-			else {
+			} else {
 				result = project.getLastCompletedBuild().getResult().toString();
 			}
-		}
-		else {
+		} else {
 			result = "NOT_BUILD";
 		}
 		return result;
 	}
 
+	public String getSoundNotif(AbstractProject project) {
+		String result = null;
+		if ((project.getLastCompletedBuild() != null)
+				&& (project.getLastCompletedBuild().getResult() != null)) {
+			if (project.isDisabled()) {
+				return null;
+			}
+
+			if (project.isBuilding()) {
+				return null;
+			}
+
+			Run lastBuild = project.getLastCompletedBuild();
+			Calendar cal = Calendar.getInstance();
+			cal.add(Calendar.SECOND, -REFRESH_PAGE_TIME);
+			if (cal.getTime().equals(lastBuild.getTime()) || cal.getTime().before(lastBuild.getTime())) {
+				Run previousBuild = project.getLastCompletedBuild()
+						.getPreviousBuild();
+				if (previousBuild.getResult() != lastBuild.getResult() && 
+						( lastBuild.getResult() == Result.FAILURE ||  lastBuild.getResult() == Result.SUCCESS)) {
+					return lastBuild.getResult().toString();
+				}
+			}
+		}
+		return result;
+	}
 
 	private int getRows() {
 		int size = getProjects().size();
 		if (size <= 3) {
 			return size;
 		}
-		return ((size % COLUMNS) == 0) ? (size / COLUMNS) : ((size + 1) / COLUMNS);
+		return ((size % COLUMNS) == 0) ? (size / COLUMNS)
+				: ((size + 1) / COLUMNS);
 	}
-
 
 	@Exported
 	public double getRowsHeight() {
 		return 100 / new Double(getRows());
 	}
-
 
 	@Exported
 	public AbstractProject[][] getProjectsArray() {
@@ -101,14 +126,12 @@ public class MonitorAction implements RootAction {
 				if (projects.size() <= 3) {
 					row = new AbstractProject[1];
 					row[0] = projects.get(i);
-				}
-				else {
+				} else {
 					// last row and uneven
 					if (((i + 1) == rows) && ((projects.size() % 2) != 0)) {
 						row = new AbstractProject[1];
 						row[0] = projects.get(i * COLUMNS);
-					}
-					else {
+					} else {
 						row = new AbstractProject[COLUMNS];
 						for (int j = 0; j < COLUMNS; j++) {
 							row[j] = projects.get((i * COLUMNS) + j);
@@ -121,10 +144,11 @@ public class MonitorAction implements RootAction {
 		return result;
 	}
 
-
 	@Exported
-	public int getStyleId(LoopTagStatus varStatus, AbstractProject[][] projectsArray) {
-		boolean lastLine = varStatus.isLast() && (projectsArray.length > 1) && (projectsArray[projectsArray.length - 1].length == 1);
+	public int getStyleId(LoopTagStatus varStatus,
+			AbstractProject[][] projectsArray) {
+		boolean lastLine = varStatus.isLast() && (projectsArray.length > 1)
+				&& (projectsArray[projectsArray.length - 1].length == 1);
 		boolean oneDimenional = (projectsArray[0].length == 1);
 		if (oneDimenional || lastLine) {
 			return 1;
--- src/test/java/hudson/plugins/statusmonitor/MonitorActionTest.java
+++ src/test/java/hudson/plugins/statusmonitor/MonitorActionTest.java
@@ -2,6 +2,8 @@ package hudson.plugins.statusmonitor;
 
 import hudson.matrix.MatrixProject;
 import hudson.maven.MavenModuleSet;
+import hudson.model.FreeStyleBuild;
+import hudson.model.Result;
 import hudson.model.AbstractProject;
 import hudson.model.FreeStyleProject;
 import org.jvnet.hudson.test.HudsonTestCase;
@@ -20,7 +22,7 @@ public class MonitorActionTest
 
         monitorAction = new MonitorAction();
     }
-
+    	
     public void testGetProjectsArray_shouldGetProjectFreeStyleProjectWithAMonitorPublisher()
             throws Exception
     {
